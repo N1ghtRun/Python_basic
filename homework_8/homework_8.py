5d1be71280b174907e39d051e6c6a6b8859f4679
N1ghtRun <dmitro.shpanchuck@gmail.com>
2022-11-20 04:34:27 +0200

    Homework 8

---
+++ homework_8/homework_8.py
@@ -0,0 +1,15 @@
+# Візьміть гру з попереднього ДЗ ('rock scissors paper lizard spock') і модифікуйте наступним чином:
+# винесіть всі функції в окремий файл (нехай буде library.py) і імпортуцте їх звідти для роботи в основний файл
+# додайте запис статистики в файл (які фігури грали і хто переміг на кожному ході), використовуйте open.
+
+
+import library
+
+
+print("\nGame of rock, scissors, paper, lizard, spock")
+
+while True:
+    library.game()
+    if not library.rematch_check():
+        print("Thanks for playing.")
+        break
